f9ed2828d0bb04601a845425b5cf44f6916ccdcf
DubeySuman <49345189+DubeySuman@users.noreply.github.com>
2025-01-24 22:48:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) ------> we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Suman")
+
+let name = "Suman"
+let age = 30
+let isLoggedIn = false
+let state;
+
+// ------------- Primitive Datatypes-------------
+
+// nmber => 2^53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof age)
