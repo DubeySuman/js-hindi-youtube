58d5ec78fafb4175b0c57f6d73e6b5ff23c249cf
DubeySuman <49345189+DubeySuman@users.noreply.github.com>
2025-01-27 01:17:57 +0000

    All types of conversions and Operations basics

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = false
+
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+
+
+// 33 => 33
+//  "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 0, -0, null, false, NaN, undefined, "" => false
+// rest all are true
+
+let someNumber = 33
+console.log(typeof someNumber)
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+// ***************** Operations *****************
+
